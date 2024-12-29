6ca39cfc3d19a6f1e6d80d1994bf5c5c872d6140
sb22844 <sb22844@gmail.com>
2024-12-29 20:37:21 +0530

    Financial analyst

---
+++ Financial AI Analyst/financial_agent.py
@@ -0,0 +1,42 @@
+from phi.agent import Agent
+from phi.model.groq import Groq
+from phi.tools.yfiannce import YFinanceTools
+from phi.tools.duckduckgo import DuckDuckGo
+import openai
+import os
+from dotenv import load_dotenv 
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+## Web search agent
+web_search_agent = Agent(
+    name = "Web Search Agent",
+    role = "Search the web for the information",
+    model = Groq(id = "llama3-groq-70b-8192-tool-use-preview"),
+    tools = [DuckDuckGo()],
+    instructions = ["Always include source"],
+    show_tools_calls = True,
+    markdown = True,
+)
+
+## Fianancial agent
+fiannce_agent = Agent(
+    name = "Fianance AI Agent",
+    model = Groq(id = "llama3-groq-70b-8192-tool-use-preview"),
+    tools = [
+        YFinanceTools(stock_price = True, analyst_recomemndations = True, stock_fundamentals = True,
+                       company_news = True),
+    ],
+    instaructions = ["Use tables to display the data"],
+    show_tools_calls = True,
+    markdown = True,
+)
+
+multi_ai_agent = Agent(
+    team = [web_search_agent, fiannce_agent],
+    instructions = ["Always include sources", "Use tables to display the data"],
+    show_tools_calls = True,
+    markdown = True,
+)
+
+multi_ai_agent.print_response("Summarize anayst recommendation and share the latest news for NVDA",stream = True)
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # PhiDataAgenticAI
+
+YT reference: https://www.youtube.com/watch?v=74SnvbQYgx8
+
+Important commands:
+setx OPENAI_API_KEY sk-dfghj
+setx GROQ_API_KEY gsk_sdfghjk
